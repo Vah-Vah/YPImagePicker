dca440e6d401c503a12016e7bc99d7cc49f0477b
dipeshpatel7733-hub <85125016+dipeshpatel7733-hub@users.noreply.github.com>
2021-06-25 03:24:56 +0530

    library changes.

--- Sources/YPImagePicker/Pages/Gallery/YPLibraryView.swift
+++ Sources/YPImagePicker/Pages/Gallery/YPLibraryView.swift
@@ -44,9 +44,9 @@ final class YPLibraryView: UIView {
     override func awakeFromNib() {
         super.awakeFromNib()
 //      assetViewContainer.multipleSelectionButton
-      downArrowImage.image = UIImage(named: "DownArrowGray")
-      multipleImage.image = UIImage(named: "MultipleImageBlack")
-      multipleBgView.layer.cornerRadius = 15
+//      downArrowImage.image = UIImage(named: "DownArrowGray")
+//      multipleImage.image = UIImage(named: "MultipleImageBlack")
+//      multipleBgView.layer.cornerRadius = 15
 //      bgView.backgroundColor = UIColor(red: 243.0/255.0, green: 243.0/255.0, blue: 243.0/255.0, alpha: 1)
         
         subviews {
